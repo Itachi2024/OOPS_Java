b06fb12afd0ab381dc491033698a7bfafdd17c76
Itachi2024 <cricaddi343@gmail.com>
2025-11-18 00:41:40 +0000

    Nov 18, 2025, 6:11 AM

--- Person.java
+++ Person.java
@@ -1,5 +1,6 @@
 // 7. Create a class Person with firstName and lastName as data members. Override the toString() method to return the full name of the person. Define constructors to take appropriate parameters.
 
 public class Person {
-    
+    private String firstName , lastName;
+
 }
